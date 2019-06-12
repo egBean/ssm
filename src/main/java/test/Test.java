9536d4b7592e7d462af8c126be38433bf5dc3064
egbean <1358404767@qq.com>
2019-06-12 16:00:31 +0800

    test提交

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,10 +1,14 @@
 package test;
 
+import cn.eg.entity.People;
 import org.apache.commons.lang3.StringUtils;
 
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println(StringUtils.isNotEmpty("daf"));
+        //System.out.println(StringUtils.isNotEmpty("daf"));
+        //String str = "abc";
+        People p = new People();
+        System.out.println(p);
     }
 }
